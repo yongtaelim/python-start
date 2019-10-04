ec67510fbbe19172df4973d45686d059bb357436
limyongtae <lyt0324@ibizsoftware.net>
2019-10-04 09:21:23 +0900

    first commit

---
+++ python-start/exam/for.py
@@ -0,0 +1,7 @@
+family = ['mother','father','son','daughter']
+
+for x in family:
+	print('%s %s' % (x, len(x)))
+    
+for i in range(4, 8):
+    print(i)
---
+++ python-start/exam/if.py
@@ -0,0 +1,7 @@
+a = 1234 * 4
+b = 13456 / 2
+
+if a > b:
+	print('a')
+else :
+	print('b')
